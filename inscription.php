27a1f256ef5ea9ad17616f48db6ec87b22e121f5
Erwan GUEGANIC <GUEGANIC_Er@lprs.local>
2024-01-15 14:37:56 +0100

    add

---
+++ inscription.php
@@ -0,0 +1,30 @@
+<?php
+var_dump($_POST);
+// Connexion à la base de données
+$bdd = new PDO("mysql:host=localhost;dbname=cinema;charset=utf8","root", "");
+
+// Vérification si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    
+    // Préparation et exécution de la requête SQL
+    $requete = $bdd->prepare("INSERT INTO user (nom, prenom, email, mdp) VALUES (?,?,?,?)");
+    $requete->execute(
+        array(
+            $_POST["nom"],
+            $_POST["prenom"],
+            $_POST["mail"],
+            $_POST["password"],
+
+        )
+    );
+    
+    // Vérification de l'insertion
+    $result = $requete->rowCount();
+    if ($result > 0) {
+        echo "Inscription réussie!";
+    } else {
+        echo "Erreur lors de l'inscription";
+    }
+}
+?>
